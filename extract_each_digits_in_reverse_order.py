4fb86da6e748a9b80293ac0b612d6165fad3c6ad
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-25 13:48:05 +0800

    Pseudocodes

--- extract_each_digits_in_reverse_order.py
+++ extract_each_digits_in_reverse_order.py
@@ -1,2 +1,10 @@
 # Exercise 11: Write a Program to extract each digit from an integer in the reverse order.
 # For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
+
+#Create a variable containing the integer
+
+#Create a loop with a certain condition to stop
+#Reverse the integer by dividing it by 10 and getting the remainder
+#Assign the quotient as the new integer to be divided
+#Display the result with spaces 
+
